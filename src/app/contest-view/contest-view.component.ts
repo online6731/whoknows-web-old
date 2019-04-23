4ffeff9bf5e63d0f2262a578a542936fc0b143d6
mehdi <mhdizmnn@gmail.com>
2019-04-23 18:07:15 +0430

    .

--- src/app/contest-view/contest-view.component.ts
+++ src/app/contest-view/contest-view.component.ts
@@ -1,32 +1,6 @@
-import { Component,
-         OnInit,
-         Injectable }
-    from '@angular/core';
-
-import { HttpClient }
-    from '@angular/common/http';
-
-import { RouterModule,
-         Router,
-         ActivatedRoute
-        }
-    from '@angular/router';
-
-import { User }
-    from '../_models/user';
-import { sendAnswerResponse }
-    from '../_models/sendAnswerResponse';
-import { viewContestResponse }
-    from '../_models/viewcontestResponse';
-    import { joinContestResponse } from '../_models/joincontestResponse';
-
-import { Contest }
-    from '../_models/contest';
-
-import { ContestService } from '../_services/contest.service';
-
-import { formatDate } from '@angular/common';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/user';
+import { Contest } from '../_models/contest';
 
 @Component({
   selector: 'app-contest-view',
@@ -34,89 +8,82 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./contest-view.component.css']
 })
 
-
-@Injectable()
 export class ContestViewComponent implements OnInit {
+  contests:          Contest[];
+  user:           User;
 
-    contest: Contest;
-    isIncontest = false;
-    teamNames = [];
-    myTeam = localStorage.getItem("username");
-    date;
+  constructor() {
+    this.contests =  [
+      {name : 'مسابقه دوره ای 132',
+     type  : 'kk',
 
-  constructor(
-        private http    	: HttpClient,
-        public  router  	: Router,
-        private route 		: ActivatedRoute,
-        private contestService : ContestService
-  ) { }
+    prize                    : {
+        coin                 :'name',
+        score                :'name',
+    },
+
+    roundsInfo                : [{
+        tags                : ['name'],
+        level                : 5,
+        start                :new Date(),
+        end                    :new Date(),
+    }],
+
+    start                    : {
+        time                : new Date(),
+        constants            : {
+            min                : 5,
+            max                : 5,
+        }
+    },
+
+    end                        : {
+        time                :new Date(),
+        constants            : {
+            min                : 5,
+            max                : 5,
+        }
+    },
+
+    join                    : {
+        time                :new Date(),
+        level                : {
+            min                : 5,
+            max                : 5,
+        },
+        score                : {
+            min                : 5,
+            max                : 5,
+        },
+        payment                : {
+            coin            : 5,
+        }
+    },
+
+    availability            : {
+        time                :new Date(),
+    },
+
+    generation                : {
+        time                :new Date(),
+    },
+
+    contestans                : [this.user],
+
+    rounds                    : [{
+        question            : [{ }], // should refer to question model
+        contestansts        : {
+            username        : {
+                answer        :'name',
+            },
+        }
+    }]
+    }
+  ];
+  }
 
 
   ngOnInit() {
-      //this.viewcontest(this.route.snapshot.params.contestID);
-      localStorage.setItem("isIncontest", "false");
   }
 
-/*
-  viewcontest(contestID: string): void{
-      this.contestService.viewcontest(contestID).subscribe(
-        (viewcontestResponse: viewcontestResponse) => {
-            if (viewcontestResponse.ok){
-
-                this.contest = viewcontestResponse.contest;
-                this.date = formatDate(this.contest.rules.start.date, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0430');
-
-                viewcontestResponse.contest.teams.forEach(function(team){
-
-                    if (team.team == localStorage.getItem("username")){
-                        localStorage.setItem("isIncontest", "true");
-                        //this.isIncontest = true;
-                        console.log('you are in contest');
-                    }
-
-                    team.members.forEach(function(player){
-
-                    });
-                });
-
-                viewcontestResponse.contest.teams.forEach(function(team){
-                    this.teamNames.push(team.team);
-                });
-
-            }
-            else {
-
-            }
-        }
-    );
-  }
-
-
-
-  joincontest(contestID: string): void{
-    this.contestService.joincontest(contestID, localStorage.getItem("username")).subscribe(
-        (joincontestResponse: joincontestResponse) => {
-            if (joincontestResponse.ok){
-
-                this.viewcontest(this.route.snapshot.params.contestID);
-
-                //this.router.navigate([`/contest/${contestID}/team/${localStorage.getItem("username")}/play`]);
-            } else {
-
-            }
-        }
-    );
-  }
-
-  opencontest(contestID: string): void {
-    this.router.navigate([`/contest/${contestID}/team/${localStorage.getItem("username")}/play`]);
-  }
-
-  incontest() {
-      return localStorage.getItem('isIncontest') == 'true';
-  }
-
-
-*/
 }
-
